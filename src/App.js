571938067ab7385b004920cae942606352a93204
Jonathon Yu <belisariuscelsus@gmail.com>
2017-11-07 08:17:04 +0000

    initial components and placeholders pending state & event handlers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GMap from './Components/Map.js';
+import Slider from './Components/Slider.js';
 import './App.css';
 
 class App extends Component {
+  
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { 
+  //     width: '0', 
+  //     height: '0' 
+  //   };
+  //   this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  // }
+  
+  //header doesn't even show :()
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header"> 
+          <h3 className="App-title">Map Demo</h3>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="flexbox">
+            <GMap/>
+        </div>
+        <Slider/>
       </div>
     );
   }
